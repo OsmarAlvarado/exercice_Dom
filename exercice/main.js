ea61e9463972cd8740bebce6c412f8a7bfa1ab9b
OsmarAlvarado <osmar.alvarado.dev@gmail.com>
2022-11-14 21:26:01 +0100

    final class

---
+++ exercice/main.js
@@ -0,0 +1,14 @@
+//---ITERATION 1
+const list1 = document.querySelector('ul')
+list1.style.backgroundColor = 'blue'
+
+const changeColor = document.querySelector('.touch')
+const title = document.querySelector('h1')
+
+changeColor.onclick = () => {
+    title.style.color = 'green'
+}
+
+console.log(list1);
+
+//--ITERATION 2
